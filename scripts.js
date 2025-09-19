b40871ca20ad0dd8852d703c89a300953cb038bb
bigotes24 <alegkey2410@gmail.com>
2025-09-19 12:50:39 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 // Captura el formulario por su id
 const form = document.getElementById("formulario");
 
-// Agrega un evento cuando el usuario envíe el formulario
+// Evento de envío del formulario
 form.addEventListener("submit", function(event) {
   event.preventDefault(); // Evita que se recargue la página
   const nombre = document.getElementById("nombre").value;
-  alert("Gracias " + nombre + ", hemos recibido tu mensaje.");
-  form.reset(); // Limpia el formulario
+  alert("Gracias " + nombre + ", hemos recibido tu mensaje. ¡Pronto nos comunicaremos contigo!");
+  form.reset(); // Limpia los campos
 });
